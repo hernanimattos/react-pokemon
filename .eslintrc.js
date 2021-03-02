4e7a16dd5cad1eb3048ff65343e799a8d0e620ec
hernanimattos <hernani.luiz@gmail.com>
2021-03-02 20:08:31 -0300

    jest and redirect

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  env: {
-    browser: true,
-  },
-  plugins: ["react"],
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  parser: "babel-eslint",
-  rules: {
-    "react/prop-types": 0,
-  },
+    env: {
+        browser: true,
+        jest: true,
+    },
+    plugins: ['react'],
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: 'babel-eslint',
+    rules: {
+        'react/prop-types': 0,
+    },
 };
